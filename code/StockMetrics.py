fa000c7b458dbf02b20cada37dd783cbced30c67
adesoye@usc.edu <adesoyee@yahoo.com>
2023-10-27 05:11:38 +0100

    full lab completed

--- code/StockMetrics.py
+++ code/StockMetrics.py
@@ -13,20 +13,53 @@ class StockMetrics(StockData):
         self.load()
 
     def average01(self):
-        """pt1 new code
-        """
+        
         averages = []
         for row in self.data:
-            print(row)
-
+            data_row=row[1:]
+            y = []
+            for x in data_row:
+                try:
+                    y.append(float(x))
+    
+                except ValueError:
+                    continue
+            average = stats.mean(y)
+            rounded_average = round(average,3)
+            averages.append(round(stats.mean(y),3))
         return averages
 
     def median02(self):
-        """pt2
-        """
-        ...
+
+        medians = []
+        for row in self.data:
+            data_row=row[1:]
+            m = []
+            for t in data_row:
+                try:
+                    m.append(float(t))
+                except ValueError:
+                    continue
+            if m:
+                median = stats.median(m)       
+                medians.append(median)
+        return medians
+        
 
     def stddev03(self):
-        """pt3
-        """
-        ...
+        sdev = []
+
+        for row in self.data:
+            data_row = row[1:]
+
+            s = []
+            for t in data_row:
+                try:
+                    s.append(float(t))
+                except ValueError:  
+                    continue
+
+            new_sd = stats.stdev(s)
+            new_sd2= round(new_sd, 3)
+            sdev.append(new_sd2)
+        return sdev
